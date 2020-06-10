f112b12ebdab9b0d7172e73b274465a216f09ae3
Saddam Akhtar <saddam.a@hcl.com>
2020-06-10 21:15:55 +0200

    Updated package manifest and folder structure

--- Sources/NotificationView.swift
+++ Sources/NotificationView.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class NotificationView: UIView {
+public class NotificationView: UIView {
     
     private var contentLeftInset: NSLayoutConstraint!
     private var contentTopInset: NSLayoutConstraint!
